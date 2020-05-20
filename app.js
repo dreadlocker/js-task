1c75c3d1e789c89d8e430e44c31159a22318537b
Svetozar <svetozariliev13@gmail.com>
2020-05-20 18:38:45 +0300

    task done

---
+++ app.js
@@ -0,0 +1,41 @@
+class Animal {
+  Sleep() {
+    console.log("zzz…");
+  }
+  Eat() {
+    console.log("nom nom…");
+  }
+}
+
+class Tiger extends Animal {
+  Roar() {
+    console.log("roar…");
+  }
+}
+
+class Dolphin extends Animal {
+  Swim() {
+    console.log("splash…");
+  }
+}
+
+class Bee extends Animal {
+  Fly() {
+    console.log("buzz…");
+  }
+}
+
+class Bird extends Animal {
+  Fly() {
+    console.log("swoosh…");
+  }
+  Sing() {
+    console.log("chirp…");
+  }
+}
+
+class Snake extends Animal {
+  Hiss() {
+    console.log("hiss…");
+  }
+}
